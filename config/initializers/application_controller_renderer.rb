8f548f1f7ad12e3fad5e913cb8731476da1b98c8
ondaruri <f3raser8cg@yahoo.co.jp>
2020-12-16 00:15:53 +0000

    1216

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'a6c4f02d1d1161798fa9e7dc0f207c32b98b7c16a34223686be0359372ba2824c19441105474a8bf8ad210b3f8563c4668edb502fe15acfd7d06076d1812a29e'
